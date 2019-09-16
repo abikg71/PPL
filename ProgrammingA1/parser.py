a1427b2d2e2ef2d9ae78777a4f52b1fc9a9ca91f
abikg71 <abinetkenore14@yahoo.com>
2019-09-16 13:19:37 -0600

    TODO initiated

--- ProgrammingA1/parser.py
+++ ProgrammingA1/parser.py
@@ -2,7 +2,18 @@
 CS 3210 - Principles of Programming Languages (Fall 2019)
 Programming Assignment 01
 September 29th, 2019
-Abinet Kenore
-Rohit Shrestha
+Team:    Abinet Kenore
+         Rohit Shrestha
 
 '''
+from enum import Enum
+import sys
+# All char classes
+# Reads the next char from input and returns its class
+# Calls getChar and getChar until it returns a non-blank
+# Adds the next char from input to lexeme, advancing the input by one char
+# All tokens
+# Error Table
+# Lexeme to token conversion
+# Returns the next (lexeme, token) pair or None if EOF is reached
+# Main
